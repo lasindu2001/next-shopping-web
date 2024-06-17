d4b930f1ac01c470ec18a2e9e98fdf3b995c6e09
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-17 14:38:20 +0530

    create product list component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const ProductList = () => {
+    return (
+        <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+            <Link
+                href="/test"
+                className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+            >
+                <div className="relative w-full h-80">
+                    <Image
+                        src="/product.png"
+                        alt=""
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                    />
+                    <Image
+                        src="/woman.png"
+                        alt=""
+                        fill
+                        sizes="25vw"
+                        className="absolute object-cover rounded-md"
+                    />
+                </div>
+                <div className="flex justify-between">
+                    <span className="font-medium">Product Name</span>
+                    <span className="font-semibold">$50</span>
+                </div>
+                <div className="text-sm text-gray-500">Description</div>
+                <button className="rounded-2xl ring-1 ring-red text-red w-max py-2 px-4 text-xs hover:bg-red hover:text-white">
+                    Add to Cart
+                </button>
+            </Link>
+        </div>
+    )
+}
+
+export default ProductList
