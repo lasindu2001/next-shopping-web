ca72bc635b464a7eb2a80f9459c80b9b95563548
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-17 10:25:21 +0530

    create navbar icons

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const CartModal = () => {
+  return (
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+        CartModal
+    </div>
+  )
+}
+
+export default CartModal
