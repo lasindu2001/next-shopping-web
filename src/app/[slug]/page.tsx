9c97b79d00aec6d90971d077a34202f4f2e3d993
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-19 22:45:32 +0530

    fetch a single product details

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,42 +1,59 @@
 import Add from '@/components/Add'
 import CustomizeProducts from '@/components/CustomizeProducts'
 import ProductImages from '@/components/ProductImages'
-import React, { Suspense } from 'react'
+import { wixClientServer } from '@/lib/wixClientServer'
+import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  const wixClient = await wixClientServer();
+
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ea architecto non ab explicabo eum, ullam vitae aperiam, doloremque consequatur laborum facilis placeat assumenda fugiat cum adipisci sint veniam asperiores?
-        </p>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
         <div className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">
-            $50
-          </h3>
-          <h2 className="font-medium text-2xl">
-            $40
-          </h2>
-        </div>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl">${product.price?.price}</h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl">
+              ${product.price?.discountedPrice}
+            </h2>
+          </div>
+        )}
         <div className="h-[2px] bg-gray-100" />
         <CustomizeProducts
         />
         <Add
         />
         <div className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum dolor consequuntur repellat error blanditiis eius delectus ut eveniet aspernatur. Sunt voluptate unde et voluptas maiores saepe natus ratione nihil cum.
-          </p>
-        </div>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className="text-sm" key={section.title}>
+            <h4 className="font-medium mb-4">{section.title}</h4>
+            <p>{section.description}</p>
+          </div>
+        ))}
         <div className="h-[2px] bg-gray-100" />
       </div>
     </div>
