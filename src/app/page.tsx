39bb1cb18e541f99f684665efe0a97690b37fa9d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-19 15:27:08 +0530

    add suspense loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import CategoryList from "@/components/CategoryList"
 import ProductList from "@/components/ProductList"
+import Skeleton from "@/components/Skeleton"
 import Slider from "@/components/Slider"
 import { useWixClient } from "@/hooks/useWixClient"
 import { wixClientServer } from "@/lib/wixClientServer"
-import { useEffect } from "react"
+import { Suspense, useEffect } from "react"
 
 const HomePage = async () => {
   // TEST (FETCHING ON THE CLIENT COMPONENT)
@@ -19,16 +20,21 @@ const HomePage = async () => {
   // }, [wixClient]);
 
   // TEST (FETCHING ON THE SERVER COMPONENT)
-  const wixClient = await wixClientServer();
-  const res = await wixClient.products.queryProducts().find();
-  console.log(res);
+  // const wixClient = await wixClientServer();
+  // const res = await wixClient.products.queryProducts().find();
+  // console.log(res);
 
   return (
     <div className=''>
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={<Skeleton/>}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -38,7 +44,12 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
     </div>
   )
